6750518df798a375d3678541837d258865729e88
makeclean <davisa@engr.wisc.edu>
2013-06-26 08:58:14 -0500

    added get_tag_data

--- retag_mesh/retag_hdf5.cpp
+++ retag_mesh/retag_hdf5.cpp
@@ -47,6 +47,8 @@ int main(int argc, char **argv)
 
   rval = MBI()->tag_get_tags(tag_list); // get the list of tag handles
 
+  moab::Tag dag_tag;
+
   for ( tl_it = tag_list.begin() ; tl_it != tag_list.end() ; ++tl_it ) // loop 
     {
       moab::TagType tmp_tagtype;
@@ -56,19 +58,48 @@ int main(int argc, char **argv)
       mbtag = *tl_it; // a given tag handle
       rval = MBI()->tag_get_name(mbtag,tmp_tagname); // get the name of the tag handle
       rval = MBI()->tag_get_type(mbtag,tmp_tagtype); // the type of the tag handle
+      if("DAGMC_TAG_NAME"==tmp_tagname)
+	dag_tag = mbtag;
       tag_types.push_back(tmp_tagtype);
       tag_names.push_back(tmp_tagname);
       std::cout << tmp_tagname << " " << tmp_tagtype << std::endl;
     }
 
+
   MBRange dagmc_mat;
   MBTag dagmc_tag;
+  std::vector<std::string> dagmc_matnum;
 
-  rval = MBI()->tag_get_handle("DAGMC_TAG_NAME",1,MB_TYPE_INTEGER,
-			       dagmc_tag,moab::MB_TAG_DENSE);
-  rval = MBI()->get_entities_by_type_and_tag(input_mesh_set,2,&dagmc_tag,0,0,
-					     dagmc_mat);
-  /// rval = MBI()->get_entities_by_type_and_tag(0,input_mesh_set,dagmc_mat,NULL,1,dagmc_mat,MBInterface::UNION);
+  //  rval = MBI()->tag_get_handle("DAGMC_TAG_NAME",1,MB_TYPE_INTEGER,
+  //			       dagmc_tag,moab::MB_TAG_DENSE);
+  rval = MBI()->tag_get_handle("NAME",0,MB_TYPE_OPAQUE,
+  			       dagmc_tag,moab::MB_TAG_DENSE);
+  if(rval!=MB_SUCCESS)
+    {
+      std::cout << "Could not get the handle" << std::endl;
+      exit(1);
+    }
+  rval = MBI()->get_entities_by_type_and_tag(input_mesh_set,MBENTITYSET
+  			      ,&dagmc_tag,NULL,1,dagmc_mat, MBInterface::UNION);
+  if(rval!=MB_SUCCESS)
+    {
+      std::cout << "Could not get the entities" << std::endl;
+      exit(1);
+    }
+  rval = MBI()->tag_get_data(dagmc_tag,dagmc_mat,&dagmc_matnum);
+    {
+      std::cout << "Could not get the tag data" << std::endl;
+      exit(1);
+    }
+
+    for( std::vector<std::string>::const_iterator i=dagmc_matnum.begin() ; i <= dagmc_matnum.end() ; i++)
+    {
+      std::cout << *i << std::endl;
+    }
+
+  std::cout << dagmc_mat << std::endl;
+  std::cout << sizeof(dagmc_mat) << std::endl;
+
   return 0;
 }
 
